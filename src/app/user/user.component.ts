c96e83837c1b99412c3212520f3bc36735097fd4
7incho <martin30990@gmail.com>
2024-02-12 15:41:50 -0300

    feat: color changer button

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,6 +13,32 @@ export class UserComponent {
   isLoggedIn = false;
   favGame = '';
 
+  bgColor = [
+    {
+      id: 1,
+      rawColor: 'Blanco',
+      colorCode: '#FDF0D1',
+    },
+    {
+      id: 2,
+      rawColor: 'Rosado',
+      colorCode: '#AC7D88',
+    },
+    {
+      id: 3,
+      rawColor: 'Tinto',
+      colorCode: '#643843',
+    },
+  ];
+
+  indiceColorActual: number = 0;
+
+  setColor(): void {
+    this.indiceColorActual = (this.indiceColorActual + 1) % this.bgColor.length;
+    document.body.style.backgroundColor =
+      this.bgColor[this.indiceColorActual].colorCode;
+  }
+
   getFavourite(gameName: string) {
     this.favGame = gameName;
   }
